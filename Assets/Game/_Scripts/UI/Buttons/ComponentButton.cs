deb49c0d235871ad4f00f5af101da04d016141c8
bariskilinc02 <developerkilinc@gmail.com>
2023-07-27 16:46:31 +0300

    #UI Update #component button names

--- Assets/Game/_Scripts/UI/Buttons/ComponentButton.cs
+++ Assets/Game/_Scripts/UI/Buttons/ComponentButton.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,9 +9,11 @@ public class ComponentButton : MonoBehaviour
 {
     public Button button;
     public int index;
-
+    public TextMeshProUGUI text;
+    
     private void Awake()
     {
+        text.text = $"{transform.parent.parent.parent.name} {index}";
         button.onClick.AddListener(OnClickedButton);
     }
 
